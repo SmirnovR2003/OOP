e37612610c7bba76abe3eaf0e3b953f97da9a936
m.smm2003mail.ru@gmail.com <m.smm2003mail.ru@gmail.com>
2023-02-12 18:52:11 +0300

    Добавил программу flipbyte.exe

--- Replace/Replace/test.bat
+++ Replace/Replace/test.bat
@@ -4,11 +4,18 @@ set Program="%~1"
 
 set OUT="%TEMP%\out.txt"
 
+%PROGRAM% > nul
+if NOT ERRORLEVEL 1 goto err
+
 %Program% 12312312345.txt %OUT% 1231234 0 || goto err
 fc %OUT% 12312312345-out.txt || goto err
 
-%PROGRAM% > nul
-if NOT ERRORLEVEL 1 goto err
+%Program% mama.txt %OUT% ma mama || goto err
+fc %OUT% mama-out.txt || goto err
+
+%Program% Empty.txt %OUT% 1231234 0 || goto err
+fc %OUT% Empty-out.txt || goto err
+
 
 echo All tests passed
 exit /B 0
---
+++ flipbyte/flipbyte/flipbyte.cpp
@@ -0,0 +1,57 @@
+﻿// flipbyte.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void toBinary(int n, string& binaryNumber)
+{
+    if (n / 2 != 0) {
+        toBinary(n / 2, binaryNumber);
+    }
+
+    binaryNumber = to_string(n % 2) + binaryNumber;
+}
+
+void FlipByte(string binaryNumber) {
+    while (binaryNumber.size() < 8) {
+        binaryNumber += "0";
+    }
+
+    int number = 0;
+    for (int i = 0; i < binaryNumber.size(); i++)//    пошли по строке бит
+        if (binaryNumber[i] == '1')   // если i-й бит в строке = 1 ,
+            number += pow(2, binaryNumber.size() - i - 1);
+
+    cout << number;
+}
+
+int main(int argc, char* argv[])
+{
+
+    if (argc != 2)
+    {
+        cout << "Неверное количество аргументов.\n"
+            "Формат ввода должен быть такой:\n"
+            "flipbyte.exe <byte>";
+        return 1;
+    }
+
+
+    int number = stoi(argv[1]);
+
+    if (number < 0 || number > 255) {
+        cout << "Произошла ошибка открытия входного файла";
+        return 1;
+    }
+
+    string binaryNumber;
+
+    toBinary(number, binaryNumber);
+    FlipByte(binaryNumber);
+
+    return 0;
+}
